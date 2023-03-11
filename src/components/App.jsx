055175765e03fdb616e2e9e33246a3b57932eaa4
Voff4eg1905 <106484238+Voff4eg1905@users.noreply.github.com>
2023-03-11 21:30:29 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,18 +3,31 @@ import Form from 'components/Form/Form';
 import Contacts from './Contacts/Contacts';
 import Filter from './Filter/Filter';
 import { nanoid } from 'nanoid';
-
+const contactsList = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
-
+  componentDidMount() {
+    const localeStorageInfo = localStorage.getItem('phonebook');
+    console.log(localeStorageInfo);
+    if (localeStorageInfo) {
+      this.setState({ contacts: JSON.parse(localeStorageInfo) });
+      return;
+    }
+    this.setState({ contacts: contactsList });
+  }
+  componentDidUpdate(_, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('phonebook', JSON.stringify(this.state.contacts));
+    }
+  }
   onFormSubmit = ({ name, number }) => {
     const contactExists = this.state.contacts.find(
       contact =>
